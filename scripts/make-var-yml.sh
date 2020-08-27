717376e319db3b954e2ed49f41fe5ce0ba89a50f
raftergit <patrickgoraft@MacBook-Pro.local>
2020-08-27 02:34:09 -0400

    resolving eslint upgrade issues

--- scripts/make-var-yml.sh
+++ scripts/make-var-yml.sh
@@ -1,4 +1,4 @@
  #!/bin/sh
 touch tdrs-backend/vars-backend.yml && touch tdrs-frontend/vars-frontend.yml
-echo docker-backend: goraftdocker/tdp-backend:$1\ > tdrs-backend/vars-backend.yml
-echo docker-frontend: goraftdocker/tdp-frontend:$1\ > tdrs-frontend/vars-frontend.yml
+echo docker-backend: goraftdocker/tdp-backend:${1}\ > tdrs-backend/vars-backend.yml
+echo docker-frontend: goraftdocker/tdp-frontend:${1}\ > tdrs-frontend/vars-frontend.yml
