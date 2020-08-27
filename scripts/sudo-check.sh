81dd5db58d27077909d58af04f9b44fe7e7c2e5a
raftergit <patrickgoraft@MacBook-Pro.local>
2020-08-27 16:15:53 -0400

    testing snyk

--- scripts/sudo-check.sh
+++ scripts/sudo-check.sh
@@ -4,6 +4,6 @@ if command -v sudo /dev/null 2>&1; then
     echo The command sudo is available
 else
     echo The command sudo is not available installing...
-    apt-get install -y sudo
+    apt-get update && apt-get install -y sudo
     ls -al /bin/sh && sudo rm /bin/sh && sudo ln -s /bin/bash /bin/sh && ls -al /bin/sh
 fi  
